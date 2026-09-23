b3281a00010d97a7c88890c3f6b9b0bf4f2ef45f
unknown <gabrielbois2003@gmail.com>
2026-09-23 14:31:17 +0200

    New modif in app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,11 @@
 const express = require('express')
-const app = express()
-const port = 3000
-const cors =  require('cors')
+const cors = require('cors')
 const helmet = require('helmet')
 const rateLimit = require('express-rate-limit')
-const path = require('path')
 
-require('dotenv').config
+require('dotenv').config()
+
+// Connexion a la bdd
 require('./config/db')
 
 //Import des routes
@@ -14,28 +13,29 @@ const userRoutes = require('./routes/userRoutes')
 const teamRoutes = require('./routes/teamRoutes')
 const conventionRoutes = require('./routes/conventionRoutes')
 
+const app = express()
+const port = 3000
+
+// Configuration Cors
 const corsOption = {
-    origin: ['http://']
+    origin: 'http://localhost:3000'
 }
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
     limit: 100,
-    message: {status: 429, error: 'Too many requests; please try again later.'}
+    message: {
+        status: 429,
+        error: 'Too many requests, please try again later.'
+    }
 })
-
-const startServer = async () => {
-    // await connectDB()
-
-    await sequelize.sync({alter: false})
-    console.log('Tables synchronized')
-}
-
 
 app.use(
     helmet({
         contentSecurityPolicy: false,
-        crossOriginResourcePolicy: { polycy: "cross-origin" }
+        crossOriginResourcePolicy: {
+            policy: 'cross-origin'
+        }
     })
 )
 
@@ -43,14 +43,17 @@ app.use(express.json())
 app.use(cors(corsOption))
 app.use(limiter)
 
-//Monte le routeur sur le chemin de base
+// Routes
+app.use('/users', userRoutes)
+app.use('/teams', teamRoutes)
+app.use('/conventions', conventionRoutes)
 
-//Démarrage du serveur
-startServer()
+// Routes d'acceuil
 app.get('/', (req, res) => {
     res.send('Welcome to Jura..TsunaCrew')
 })
 
+//Démarrage du serveur
 app.listen(port, () => {
-    console.log(`Serveur démaré sur http://localohost:${port}`)
+    console.log(`Serveur démarré sur http://localhost:${port}`)
 })
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const router = express.router()
+const router = express.Router()
 const { register, login } = require('../controllers/userController')
 
 router.post('/register', register)
